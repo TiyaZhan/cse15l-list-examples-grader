c941b3a3d6dbe9f43fe00ca6236193a5a1d0a812
TiyaZhan <tizhan@ucsd.edu>
2022-11-22 15:44:02 -0800

    complete grade.sh and test

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,71 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
 
+class LengthChecker implements StringChecker{
+  @Override
+  public boolean checkString(String s){
+    return s.length()>3;
+  }
+}
 public class TestListExamples {
   // Write your grading tests here!
+  @Test
+  public void testSimpleFilter(){
+    ArrayList<String> input = new ArrayList<>();
+    input.add("abd");
+    input.add("abcd");
+    input.add("aaaaaa");
+    input.add("bbbbb");
+    ArrayList<String> exp = new ArrayList<>();
+    exp.add("abcd");
+    exp.add("aaaaaa");
+    exp.add("bbbbb");
+    assertEquals(exp, ListExamples.filter(input, new LengthChecker()));
+  }
+
+  @Test 
+	public void testFilter() {
+    ArrayList<String> input = new ArrayList<String>();
+    ArrayList<String> exp = new ArrayList<String>();
+    input.add("a");
+    input.add("abcd");
+    input.add("accd");
+    exp.add("abcd");
+  
+    exp.add("accd");
+    ArrayList<String> result = (ArrayList<String>)ListExamples.filter(input, new LengthChecker());
+    assertEquals(exp.get(1), result.get(1));
+	}
+
+  @Test(timeout = 1000)
+  public void testSimpleMerge(){
+    ArrayList<String> input1 = new ArrayList<>();
+    ArrayList<String> input2 = new ArrayList<>();
+    ArrayList<String> exp = new ArrayList<>();
+
+    input1.add("a");
+    input2.add("b");
+    exp.add("a");
+    exp.add("b");
+    assertEquals(exp, ListExamples.merge(input2, input1));
+  }
+  @Test(timeout = 1000)
+  public void testMerge()
+  {
+    ArrayList<String> input1 = new ArrayList<String>();
+    ArrayList<String> input2 = new ArrayList<String>();
+
+    input1.add("a");
+    input1.add("c");
+    input2.add("b");
+    input2.add("d");
+    ArrayList<String> exp = new ArrayList<String>();
+    exp.add("a");
+    exp.add("b");
+    exp.add("c");
+    exp.add("d");
+
+    assertEquals(exp, ListExamples.merge(input1, input2));
+    }
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,55 @@
 # Create your grading script here
 
-set -e
+rm -rf student-submission 
 
-rm -rf student-submission
-git clone $1 student-submission
+git clone $1 student-submission 
 
+CPATH=".:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar"
+cd student-submission 
+if [[ -e ListExamples.java ]]
+then
+    echo "Correct files submitted" 
+else
+    echo "Wrong files submitted! Score 0/4"
+    exit
+fi
+
+cp ../TestListExamples.java ./ 
+javac -cp $CPATH *.java 
+if [[ $? -eq 0 ]]
+then   
+    echo "Successfully Compiled" 
+else   
+    echo "Compile Error! Score 0/4" 
+    exit
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > ../result.txt
+
+cd ../
+grep "Failures: 1" result.txt
+if [[ $? -eq 0 ]]
+then 
+    echo "Score: 3/4"
+    exit
+fi
+grep "Failures: 2" result.txt
+if [[ $? -eq 0 ]]
+then 
+    echo "Score: 2/4"
+    exit
+fi
+grep "Failures: 3" result.txt
+if [[ $? -eq 0 ]]
+then 
+    echo "Score: 1/4"
+    exit
+fi
+grep "Failures: 4" result.txt
+if [[ $? -eq 0 ]]
+then 
+    echo "Score: 0/4"
+    exit
+fi
+
+echo "Great Work! Score: 4/4"
